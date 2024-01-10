0ab74998bedf5cc0df258586201fe8221c643c52
mrathore14 <mahipal.singh@metlife.com>
2024-01-10 16:10:10 +0530

    salesForce

--- src/com/metlife/basics/Demo4SalesForce.java
+++ src/com/metlife/basics/Demo4SalesForce.java
@@ -23,16 +23,23 @@ public class Demo4SalesForce
 
         //enter firstname as john
         driver.findElement(By.name("UserFirstName")).sendKeys("john");
+        driver.findElement(By.name("UserLastName")).sendKeys("rathore");
         //enter emailaddress as john@CCCS.COM
         driver.findElement(By.name("UserEmail")).sendKeys("john@cccs.com");
+        driver.findElement(By.name("CompanyName")).sendKeys("Metlife");
+        driver.findElement(By.name("UserPhone")).sendKeys("8387825086");
+        Select country=new Select(driver.findElement(By.name("CompanyCountry")));
+        country.selectByVisibleText("Maldives");
         //select jobtitle as "IT Manager"
         Select jobTitle=new Select(driver.findElement(By.name("UserTitle")));
         jobTitle.selectByVisibleText("IT Manager");
         //select employees to '101-200 emplpoyees'
         Select emp=new Select(driver.findElement(By.name("CompanyEmployees")));
         emp.selectByVisibleText("101 - 200 employees");
+
         //click on checkbox of agreement
         driver.findElement(By.xpath("(//div[@class='checkbox-ui'])[2]")).click();
 
+        driver.findElement(By.name("start my free trial")).click();
     }
 }
