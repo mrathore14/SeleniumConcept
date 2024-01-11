959a23ef14ad043f49520cebd365faf3158aede4
mrathore14 <mahipal.singh@metlife.com>
2024-01-11 15:05:48 +0530

    db4Free

--- src/com/metlife/tabs/Demo1DBFree.java
+++ src/com/metlife/tabs/Demo1DBFree.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.Set;
 
 public class Demo1DBFree
 {
@@ -17,6 +18,22 @@ public class Demo1DBFree
 
         driver.findElement(By.xpath("//a[@href='/phpMyAdmin']")).click();
 
+        String parentWindowHandle = driver.getWindowHandle();
+        driver.findElement(By.linkText("phpMyAdmin")).click();
+
+
+        Set<String> allWindowHandles = driver.getWindowHandles();
+        for (String windowHandle : allWindowHandles)
+         {
+          if (!windowHandle.equals(parentWindowHandle))
+          {
+            driver.switchTo().window(windowHandle);
+          break;
+          }
+        }
+        driver.findElement(By.id("input_username")).sendKeys("Mahipal");
+        driver.findElement(By.id("input_password")).sendKeys("rathore");
+        driver.findElement(By.id("input_go")).click();
 
     }
 }
